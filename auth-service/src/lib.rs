2be0fe8c7e049132293bb7464b3f5e66befe77b3
Bonno van der Basch <git@vanderbasch.com>
2026-02-05 15:05:51 +0100

    [Sprint 1][Task 4] chore: complete sprint task 4
    * Add context for AI agent
    * Update routes and corresponding tests
    * Introduce slumber for API endpoint testing
    * Update taskfile with tasks for API tests
    * Update git ignore rules to ensure service logs are not submitted to version control

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,25 +1,30 @@
 use std::error::Error;
 
-use axum::{serve::Serve, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Router};
 use tokio::net::TcpListener;
-use tower_http::services::ServeDir;
+use tower_http::services::{ServeDir, ServeFile};
 
-// This struct encapsulates our application-related logic.
 pub struct Application {
     server: Serve<TcpListener, Router, Router>,
     pub address: String,
 }
 
 impl Application {
-    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
-        let assets_dir = ServeDir::new("assets");
-        let router = Router::new().fallback_service(assets_dir);
+    pub async fn build(address: &str) -> Result<Application, Box<dyn Error>> {
+        let asset_dir =
+            ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
+        let router = Router::new()
+            .fallback_service(asset_dir)
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/logout", post(logout))
+            .route("/verify-2fa", post(verify2fa))
+            .route("/verify-token", post(verifytoken));
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
         let server = axum::serve(listener, router);
 
-        // Create a new Application instance and return it
         Ok(Self { server, address })
     }
 
@@ -27,4 +32,24 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
+}
+
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verifytoken() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
