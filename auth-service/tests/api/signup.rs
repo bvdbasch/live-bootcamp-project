3a93a895457897ffffdc84cf79fe80bab95c7ee1
Bonno van der Basch <git@vanderbasch.com>
2026-02-06 16:49:29 +0100

    [Sprint 1][Task 6] chore: complete sprint task 6
    * Add context for AI agent
    * Update routes and corresponding tests
    * Update slumber with request bodies
    * Update taskfile with to include new flavors for API endpoints (test all response codes)

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,11 +1,24 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
 
-// Tokio's test macro is used to run the test in an async environment
 #[tokio::test]
-async fn submitting_signup_returns_200() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
+    let random_email = get_random_email(); // Call helper method to generate email
 
-    let response = app.post_signup().await;
+    let test_cases = [
+        serde_json::json!({ "password": "password123","requires2FA": true }),
+        serde_json::json!({ "requires2FA": true }),
+        serde_json::json!({ "password": "password123"}),
+        serde_json::json!({ "email": "mail@example.letsgetrusty.com"}),
+    ];
 
-    assert_eq!(response.status().as_u16(), 200);
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await; // call `post_signup`
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
 }
