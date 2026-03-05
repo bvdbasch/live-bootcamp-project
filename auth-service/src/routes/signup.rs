91d1ce040910470e9b76d83ab898a9ffdf1bc958
Bonno van der Basch <git@vanderbasch.com>
2026-03-05 14:30:02 +0100

    [Sprint 2][Task 2] chore: complete sprint task 2
    * Add context for AI agent
    * Add test cases for invalid data and existing users
    * Add error enum to domain module
    * Implement Axum intoResponse
    * Update signup route
    * Update slumber and taskfile with the new test cases introduced in this commit
    * Housekeeping on use statements and modules

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use axum::{
     extract::{self, State},
     http::StatusCode,
@@ -8,7 +6,10 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 
-use crate::{app_state::AppState, domain::User, services::HashmapUserStore};
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, User},
+};
 
 #[derive(Deserialize)]
 pub struct SignupRequest {
@@ -23,27 +24,45 @@ pub struct SignupResponse {
     pub message: String,
 }
 
-// Use Axum's state extractor to pass in AppState
 pub async fn signup(
     State(state): State<AppState>,
     Json(request): Json<SignupRequest>,
-) -> impl IntoResponse {
-    // Create a new `User` instance using data in the `request`
-    let user: User = User::new(request.email, request.password, request.requires_2fa);
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email = request.email;
+    let password = request.password;
 
-    // Acquire a write lock on the shared user store
+    // return AuthAPIError::InvalidCredentials if:
+    // email is empty
+    if email.is_empty() {
+        return Err(AuthAPIError::InvalidCredentials);
+    }
+
+    // email does not contain '@'
+    if !email.contains("@") {
+        return Err(AuthAPIError::InvalidCredentials);
+    }
+
+    // password is less than 8 characters
+    if password.len() < 8 {
+        return Err(AuthAPIError::InvalidCredentials);
+    }
+
+    let user = User::new(email, password, request.requires_2fa);
     let mut user_store = state.user_store.write().await;
 
-    // Add `user` to the `user_store`. Simply unwrap the returned `Result` enum type for now.
-    let _ = user_store.add_user(user).unwrap();
+    // return AuthAPIError::UserAlreadyExists if email exists in user_store.
+    if user_store.get_user(&user.email).is_ok() {
+        return Err(AuthAPIError::UserAlreadyExists);
+    }
+
+    // return AuthAPIError::UnexpectedError if add_user() fails.
+    if user_store.add_user(user).is_err() {
+        return Err(AuthAPIError::UnexpectedError);
+    }
 
     let response = Json(SignupResponse {
         message: "User created successfully!".to_string(),
     });
 
-    (StatusCode::CREATED, response)
+    Ok((StatusCode::CREATED, response))
 }
-
-// pub async fn signup(Json(request): Json<SignupRequest>) -> impl IntoResponse {
-//     StatusCode::OK.into_response()
-// }
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,4 +1,5 @@
 use crate::helpers::{get_random_email, TestApp};
+use auth_service::{routes::SignupResponse, ErrorResponse};
 
 #[tokio::test]
 async fn should_return_422_if_malformed_input() {
@@ -36,5 +37,70 @@ async fn should_return_201_if_valid_input() {
         201,
         "Failed for input: {:?}",
         response
+    );
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+
+    // Create an array of invalid inputs.
+    let invalid_inputs = [
+        // empty email
+        serde_json::json!({ "email": "", "password": "password123","requires2FA": true}),
+        // email does not contain @
+        serde_json::json!({ "email": "", "password": "password123","requires2FA": true}),
+        // password smaller than 8 chars
+        serde_json::json!({ "email": get_random_email(), "password": "1234","requires2FA": true}),
+    ];
+
+    // Then, iterate through the array and make HTTP calls to the signup route.
+    //Assert a 400 HTTP status code is returned.
+    for body in invalid_inputs.iter() {
+        println!("{body}");
+        let response = app.post_signup(body).await;
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "Failed for input: {:?}",
+            body
+        );
+        assert_eq!(
+            response
+                .json::<ErrorResponse>()
+                .await
+                .expect("Could not deserialize response body to ErrorResponse")
+                .error,
+            "Invalid credentials".to_owned()
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_409_if_email_already_exists() {
+    // Call the signup route twice. The second request should fail with a 409 HTTP status code
+    let app = TestApp::new().await;
+    let random_email = get_random_email();
+    let req_body =
+        serde_json::json!({ "email": random_email, "password": "password123","requires2FA": true});
+
+    // Should create user
+    let response = app.post_signup(&req_body).await;
+    assert_eq!(
+        response.status().as_u16(),
+        201,
+        "Failed for input: {:?}",
+        response
+    );
+
+    let response = app.post_signup(&req_body).await;
+    assert_eq!(response.status().as_u16(), 409);
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "User already exists".to_owned()
     );
 }
